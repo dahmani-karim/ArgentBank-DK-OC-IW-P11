8cfb0d7ad5f369f4e0900017b7d5a5dda8f52d3f
Karim Dahmani <karim.dahmani88@gmail.com>
2024-04-03 14:31:03 +0200

    Update react-router-dom to version 6.22.3

--- argent-bank/src/main.jsx
+++ argent-bank/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Router, Routes, Route } from 'express'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
