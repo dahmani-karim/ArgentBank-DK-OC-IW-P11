2943636915ada5b54b45697f57b5f90c7ed77d6f
Karim Dahmani <karim.dahmani88@gmail.com>
2024-04-12 10:48:22 +0200

    Create EditMenu component, enable/disable input fields, and update button class names

--- argent-bank/src/components/editMenu/EditMenu.jsx
+++ argent-bank/src/components/editMenu/EditMenu.jsx
@@ -1,51 +1,40 @@
 // EditMenu component to edit the userName
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+//import { useDispatch } from "react-redux";
 import Button from "../button/Button";
 
 const EditMenu = () => {
     const pseudo = useSelector(state => state.userSlice.userProfil.userName);
     const firstName = useSelector(state => state.userSlice.userProfil.firstName);
     const lastName = useSelector(state => state.userSlice.userProfil.lastName);
-    const dispatch = useDispatch();
+    //const dispatch = useDispatch();
 
     return (
         <div>
             <h1>Edit user info</h1>
-            <form>
+            <form className="edit-menu">
                 <div className="input-wrapper">
                     <label htmlFor="username">User name:</label>
                     <input type="text" id="username" placeholder={pseudo}/>
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="firstname">First name:</label>
-                    <input type="text" id="firstname" placeholder={firstName}/>
+                    <input type="text" id="firstname" placeholder={firstName} disabled/>
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="lastname">Last name:</label>
-                    <input type="text" id="lastname" placeholder={lastName}/>
+                    <input type="text" id="lastname" placeholder={lastName} disabled/>
                 </div>
                 <Button
-                    className="sign-in-button"
+                    className="edit-button"
                     type="submit"
-                    onClick={
-                        dispatch({
-                            type: "getUserInfo",
-                            payload: {
-                                email: pseudo,
-                                firstName: firstName,
-                                lastName: lastName,
-                                userName: document.getElementById('username').value
-                            }
-                        })
-                    }
                     btnText="Save"
                 >
                 </Button>
                 <Button
-                    className="sign-in-button"
+                    className="edit-button"
                     type="submit"
-                    onClick={window.location.href = "/"}
+                    // onClick={window.location.href = "/"}
                     btnText="Cancel"
                 >
                 </Button>
